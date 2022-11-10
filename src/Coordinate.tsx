4d17660ae74e532bb123a39a29855b95ffd763e2
Dana <dana.hong@hotmail.com>
2022-11-09 22:24:14 -0500

    Update Coordinate component

--- src/Coordinate.tsx
+++ src/Coordinate.tsx
@@ -1,17 +1,31 @@
+import { useContext } from "react";
+import LocationContext from "./locationContext";
+
 import "./coordinate.scss";
 
 export default function Coordinate(props: {
-    latitudeCoordinate: string;
-    longitudeCoordinate: string;
+    location: string;
+    targeted: null | boolean;
+    occupied: null | boolean;
 }) {
-    const { latitudeCoordinate, longitudeCoordinate } = props;
+    const { location, targeted, occupied } = props;
+    const locationContext = useContext(LocationContext);
     return (
         <div
-            className="coordinate"
-            id={`${latitudeCoordinate}${longitudeCoordinate}`}
-        >
-            {/* {latitudeCoordinate}
-            {longitudeCoordinate} */}
-        </div>
+            className={`coordinate ${props.occupied}`}
+            id={location}
+            // onClick={(event) =>
+            //     locationContext.placeShip(
+            //         locationContext.selectedShip,
+            //         event.target.id
+            //     )
+            // }
+            onClick={(event) =>
+                console.log((event.target as HTMLDivElement).id)
+            }
+            onMouseOver={(event) =>
+                console.log((event.target as HTMLDivElement).id)
+            }
+        ></div>
     );
 }
